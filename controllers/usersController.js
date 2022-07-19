04564f470594c125c77c94c611914e7a87ebc631
Andrei Radu <andreiradu@Andreis-MacBook-Pro`>
2022-07-20 01:07:35 +0300

    finished addRoles,removeRoles

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -42,8 +42,7 @@ const getUserById = async (req, res) => {
 
 const addUserRoles = async (req, res) => {
   const ROLES_LIST = require("../config/roles_list");
-  const { roleName,id } = req.body;
-
+  const { roleName, id } = req.body;
   console.log("roleName", roleName);
 
   if (!roleName || !id) {
@@ -84,20 +83,86 @@ const addUserRoles = async (req, res) => {
     return res.status(400).json({ message: "Role name/s not found!" });
   }
 
-  const updatedUser = await User.findOne({_id : id}).exec();
-  console.log('updatedUser result',updatedUser);
+  const updatedUser = await User.findOne({ _id: id }).exec();
+  console.log("find updatedUser", updatedUser);
 
-  if(!updatedUser) {
-    return res.status(204).json({message: `No user found for this ID ${id}`});
+  if (!updatedUser) {
+    return res.status(204).json({ message: `No user found for this ID ${id}` });
   }
 
+  
+  const updatedUserJSObj = updatedUser.toObject();
+  console.log('before updating updatedUserJSObj', updatedUserJSObj);
 
-  console.log('start update roles');
-  updatedUser.roles = uniqueRoles;
-  updatedUser.save();
-  console.log('stop update roles');
+  uniqueRoles?.forEach(role => {
+    return updatedUserJSObj.roles = {
+      ...updatedUserJSObj.roles,
+      ...role
+    }
+  })
 
-  return res.status(200).json({message : `User with ID ${id} was updated successfully`});
+  console.log('after updating updatedUserJSObj', updatedUserJSObj);
+
+  updatedUser.roles = updatedUserJSObj.roles;
+
+  await updatedUser.save();
+
+  return res
+    .status(200)
+    .json({ message: `User with ID ${id} was updated successfully` , updatedUser});
+};
+
+const removeUserRole = async (req, res) => {
+  const { id, role } = req.body;
+
+  if (!id || !role) {
+    return res.status(400).json({ message: "ID and Role are required!" });
+  }
+
+  // find user based on id;
+  const userFind = await User.findOne({ _id: id }).exec();
+
+  if (!userFind) {
+    return res
+      .status(204)
+      .json({ message: `There is not user with this ID: ${id}` });
+  }
+
+  // transform received roles : capitalize first letter;
+  const rolesReceived = Array.isArray(role) ? role : [role];
+  const loweredRoles = rolesReceived.map((role) => role.toLowerCase());
+  const rolesArray = loweredRoles.map(
+    (role) => role.charAt(0).toUpperCase() + role.slice(1)
+  );
+
+  const uniqueCodes = [];
+  const uniqueRoles = rolesArray.filter((element) => {
+    const isDuplicate = uniqueCodes.includes(Object.values(element)[0]);
+    if (!isDuplicate) {
+      uniqueCodes.push(Object.values(element)[0]);
+      return true;
+    }
+    return false;
+  });
+
+  const assignedUserRoles = uniqueRoles.filter((role) => {
+    role = role.toLowerCase();
+    role = role.charAt(0).toUpperCase() + role.slice(1);
+    if (userFind.roles[role]) {
+      return role;
+    }
+  });
+
+  if (assignedUserRoles?.length === 0) {
+    return res.json({ message: `No roles found assigned to this user ${id}` });
+  } else {
+    const userFindJSObject = userFind.toObject();
+    assignedUserRoles.forEach((assigned) => delete userFindJSObject.roles[assigned]);
+
+    userFind.roles = userFindJSObject.roles;
+    const saveResponse = await userFind.save();
+    return res.json(saveResponse);
+  }
 };
 
 module.exports = {
@@ -105,4 +170,5 @@ module.exports = {
   deleteUser,
   getUserById,
   addUserRoles,
+  removeUserRole,
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -7,14 +7,21 @@ const {
   getAllUsers,
   deleteUser,
   getUserById,
-  addUserRoles
+  addUserRoles,
+  removeUserRole
 } = require("../../controllers/usersController");
 
+// ROUTE / => (GET) getAllUsers, (POST) addUserRoles, (DELETE) deleteUser
 router
   .route("/")
   .get(verifyRoles(ROLES_LIST.Admin), getAllUsers)
   .post(verifyRoles(ROLES_LIST.Admin),addUserRoles)
   .delete(verifyRoles(ROLES_LIST.Admin), deleteUser);
+
+router
+  .route('/removeRole')
+  .get(verifyRoles(ROLES_LIST.Admin), getAllUsers)
+  .post(verifyRoles(ROLES_LIST.Admin),removeUserRole);
 
 router.route("/:id").get(verifyRoles(ROLES_LIST.Admin),getUserById);
 
